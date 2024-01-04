eb030967f9624e6a61f35ea1396ea7485bfc3714
Guilherme <alonsogui011@gmail.com>
2024-01-04 11:36:29 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Titulo from "./Titulo"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Contato from "./pages/Contato"
 
 function App() {
   return (
     <div>
-      <Titulo cor="red" />
-      <Titulo cor="blue"/>
-      <Titulo cor="green"/>
-      <Titulo cor="purple"/>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/sobre" element={<Sobre />} />
+              <Route path="/contato" element={<Contato />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   )
 }
