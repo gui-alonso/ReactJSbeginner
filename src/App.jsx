30c8cdd920ff17434321f0959af6048b99c8644e
Guilherme <alonsogui011@gmail.com>
2024-01-04 11:18:06 -0300

    reactJs beginner

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from "./Titulo"
+
+function App() {
+  return (
+    <div>
+      <Titulo cor="red" />
+      <Titulo cor="blue"/>
+      <Titulo cor="green"/>
+      <Titulo cor="purple"/>
+    </div>
+  )
+}
+
+export default App
